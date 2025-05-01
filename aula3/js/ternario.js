945d128efeabdf6f3fe6e880434d5d45e58f8e89
lcsvinhas <l.vinhas.lv@gmail.com>
2025-04-30 21:45:59 -0300

    criei aula3

---
+++ aula3/js/ternario.js
@@ -0,0 +1,7 @@
+let exame = true;
+
+console.log(exame ? "Positivo" : "Negativo");
+
+// Média de nota superior a 70 bom, acima de 90 ótimo, abaixo de 70 reprovado
+let nota = 75;
+console.log(nota >= 90 ? "Ótimo" : nota >= 70 ? "Bom" : "Reprovado");
